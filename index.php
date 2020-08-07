dfcdba0e1bb5b31f361949adbb8dae02cee1700c
Subhasish10 <palsubhasish6@gmail.com>
2020-08-08 00:30:25 +0530

    2nd commit

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if ($uploadOk == 0) {
 <body>
 
 <form action="#" method="post" enctype="multipart/form-data">
-  Select image to upload:
+  Select image file to upload:
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload Image" name="submit">
 </form>
